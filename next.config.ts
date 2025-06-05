eafe4d1582a94d66a69436d413e7b452c95acb39
ElhamNakhkoob <elham_mn14@yahoo.com>
2025-06-05 13:33:29 +0200

    save changes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/FindFood",
+  assetPrefix: "/FindFood/",
 };
 
 export default nextConfig;
